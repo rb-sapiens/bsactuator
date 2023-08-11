3f2af76d6d418b8206e4b0e54642f353c6cb163e
Shun0750 <nagaoshun@gmail.com>
2023-08-11 21:37:15 +0900

    Disabling nonblocking

--- bsactuator/bsactuator.py
+++ bsactuator/bsactuator.py
@@ -18,18 +18,25 @@ class BsActuator:
     sys.exit(0)
 
   def set_length(self, length, speed):
-    if length < 0 or length > 2000 or (not isinstance(length, int)):
-      return False
-    if speed < 1 or speed > 10 or (not isinstance(speed, int)):
-      return False
+    if self.talking == False:
+      self.talking = True
+      req_message = "set:"+str(length)+","+str(speed)+";"
+      version = sys.version_info[0]
+      if version == 2:
+        self.ser.write(bytes(req_message).encode("utf-8"))
+      if version == 3:
+        self.ser.write(bytes(req_message, "utf-8"))
 
-    req_message = "set:"+str(length)+","+str(speed)+";"
-    version = sys.version_info[0]
-    if version == 2:
-      self.ser.write(bytes(req_message).encode("utf-8"))
-    if version == 3:
-      self.ser.write(bytes(req_message, "utf-8"))
-    return True
+      # wait until completion
+      while True:
+        if self.ser.in_waiting > 0:
+          recv_data = self.ser.readline()
+          message = recv_data.strip().decode('utf-8')
+          if "complete:" in message:
+            length_str = message.replace("complete:", "")
+            if length == int(length_str):
+              self.talking = False
+              return True
 
   def get_length(self):
     self.ser.write(b"get;")
