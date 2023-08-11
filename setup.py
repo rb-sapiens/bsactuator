6561dee0f8dc3add33785090454cf59ac6e63451
Shun0750 <nagaoshun@gmail.com>
2023-08-11 22:05:33 +0900

    Changing build bsactuator.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="bsactuator", # Replace with your own username
-    version="0.0.3",
+    version="0.0.4",
     install_requires=[
         "pyserial",
     ],
